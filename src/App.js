d17e1c62e774d680fefde52f6d79d0cc9cad8200
Daniel Muñoz <damuru88@gmail.com>
2019-02-02 12:19:46 +0100

    import counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Counter from "./components/counter";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <h1>React Hooks Exercises</h1>
         </header>
+        <div className="exercises">
+          <h3>Exercise 1 - Counter</h3>
+          <Counter />
+        </div>
       </div>
     );
   }
